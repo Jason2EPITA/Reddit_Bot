23353368f55ba62df17997defb2f6da5dde7fa5e
Jason Perez <jasonperez@mbp-de-jason.home>
2024-06-08 15:33:46 +0200

    ajout gpt integrer a l'ecoute continue

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 import praw
 from datetime import datetime
+import requests
+from utils import print_post_details, ask_bettergpt
 # Charger les variables d'environnement à partir du fichier .env
 load_dotenv()
 
@@ -10,6 +12,10 @@ username = os.getenv("USERNAME")
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 password = os.getenv("PASSWORD")
+
+
+gpt_key = os.getenv("GPT_KEY")
+gpt_url = os.getenv("GPT_URL")
 
 reddit_instance = praw.Reddit(
     client_id=client_id,
@@ -20,9 +26,6 @@ reddit_instance = praw.Reddit(
 )
 
 # print(reddit_instance.user.me())
-# Récupérer les 5 derniers posts dans la communauté r/testingground4bots
-subreddit = reddit_instance.subreddit("testingground4bots")
-recent_posts = subreddit.new(limit=5)
 
 # Récupérer les IDs des commentaires auxquels le bot a déjà répondu
 replied_comments = set()
@@ -38,44 +41,35 @@ with open(replied_posts_file, "r") as file:
     for line in file:
         replied_posts.add(line.strip())
 
-# Parcourir les 5 derniers posts
-for post in recent_posts:
-    # Vérifier si vous avez déjà répondu à ce post
-    if post.id in replied_posts:
-        continue
+# Écouter en permanence les nouveaux posts dans la communauté r/all
+communitie = "testingground4bots" # Écouter les nouveaux posts dans la communauté r/all (all c toute les communautés)
+subreddit = reddit_instance.subreddit(communitie)
+for post in subreddit.stream.submissions(skip_existing=True):
+    # print_post_details(post)
+# MATHS TEST
+    if post.title.endswith("probleme") and post.id not in replied_posts:
+        question = post.selftext
+        response = ask_bettergpt(question, gpt_key, gpt_url, "gpt-4o")
+        print(f"LA REPONSE : {response}")        
+        
+        post.reply(response)
+        
+        print("Répondu au post:", post.title)
+        print("*************")
+        
+        replied_posts.add(post.id)
 
-    post.comments.replace_more(limit=None)
-    # print("Titre du post:", post.title)
-    # print("Texte du post:", post.selftext)  # Afficher le texte du post
-    # print("URL du post:", post.url)          # Afficher l'URL du post
-    if post.selftext.endswith("quoi"):
+# LMAO TEST 
+    if post.selftext.endswith("quoi ?") and post.id not in replied_posts:
         # Répondre au post lui-même
         post.reply("quoicoubeh au post!")
         print("Répondu au post:", post.title)
         print("*************")
+
         # Ajouter l'identifiant du post à l'ensemble des posts déjà répondu
         replied_posts.add(post.id)
+
 # Enregistrer les identifiants des posts auxquels vous avez répondu dans le fichier
 with open(replied_posts_file, "w") as file:
     for post_id in replied_posts:
         file.write(post_id + "\n")
-
-
-# Fonction pour afficher les détails du post
-def print_post_details(post):
-    author = post.author.name if post.author else "N/A"
-    created_utc = datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S')
-    print("*****Nouveau post détecté*****")
-    # print(f"** Nouveau post détecté **")
-    print(f"Titre: {post.title}")
-    print(f"Auteur: {author}")
-    print(f"Date de création (UTC): {created_utc}")
-    # print(f"URL: {post.url}")
-    print(f"Texte du post: {post.selftext}")
-    print("\n")
-
-# Écouter en permanence les nouveaux posts dans la communauté r/all
-communitie = "all" # Écouter les nouveaux posts dans la communauté r/all (all c toute les communautés)
-subreddit = reddit_instance.subreddit(communitie)
-for post in subreddit.stream.submissions(skip_existing=True):
-    print_post_details(post)
---
+++ utils.py
@@ -0,0 +1,48 @@
+import datetime
+import requests
+
+# Fonction pour afficher les détails du post
+def print_post_details(post):
+    author = post.author.name if post.author else "N/A"
+    created_utc = datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S')
+    print("*****Nouveau post détecté*****")
+    # print(f"** Nouveau post détecté **")
+    print(f"Titre: {post.title}")
+    print(f"Auteur: {author}")
+    print(f"Date de création (UTC): {created_utc}")
+    # print(f"URL: {post.url}")
+    print(f"Texte du post: {post.selftext}")
+    print("\n")
+
+
+# Fonction pour demander à Better GPT
+def ask_bettergpt(question, gpt_key, gpt_url, model):
+    headers = {
+        "Authorization": f"Bearer {gpt_key}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "model": model,
+        "messages": [
+            {
+                "role": "system",
+                "content": "Vous êtes",
+                "author": "assistant"
+            },
+            {
+                "role": "user",
+                "content": question,
+                "author": "user"
+            }
+        ]
+    }
+
+    response = requests.post(gpt_url, json=data, headers=headers)
+
+    if response.status_code == 200:
+        response_data = response.json()
+        answer = response_data["choices"][0]["message"]["content"]
+        return answer
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
